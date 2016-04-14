ba481ed70b832c58a073215ec9b4c62947ebb093
Alvin Andal <alvin.009.andal@gmail.com>
2016-04-14 13:47:16 -0700

    Added examples to works

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,8 +2,8 @@ var navbarState = 0; // 0 = hidden, 1 = shown
 
 var showNavbar = function() {
     navbarState = 1;
-    $('.navbar .about').slideDown("slow");
-    $('.navbar .works').slideDown("slow");
+    $('.navbar .about').slideDown("fast");
+    $('.navbar .works').slideDown("fast");
 };
 
 var hideNavbar = function() {
@@ -12,9 +12,16 @@ var hideNavbar = function() {
     $('.navbar .works').slideUp("slow");
 };
 
+var fadeTitle = function() {
+    $('.main-content .main-title').fadeIn(2250);
+};
+
 $(window).ready(function() {
     $('.navbar .about').hide();
     $('.navbar .works').hide();
+    $('.main-content .main-title').hide();
+    
+    fadeTitle();
     
     $('.navbar').on("mouseover", function() {
         if (navbarState == 0) {
